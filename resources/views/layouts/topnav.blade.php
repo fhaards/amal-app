68e66bc02a911be1529cbe57f7319a6e50677ee5
fhaards <m.fahmi37@gmail.com>
2021-11-22 06:49:49 +0700

    first commit

---
+++ resources/views/layouts/topnav.blade.php
@@ -0,0 +1,29 @@
+<section class="w-full px-8 text-gray-700 bg-white">
+    <div class="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
+        <div class="relative flex flex-col md:flex-row">
+            <a href="#_"
+                class="flex items-center mb-5 font-medium text-gray-900 lg:w-auto lg:items-center lg:justify-center md:mb-0">
+                <span class="mx-auto text-xl font-black leading-none text-gray-900 select-none">tails<span
+                        class="text-green-400">.</span></span>
+            </a>
+            <nav
+                class="flex flex-wrap items-center mb-5 text-base md:mb-0 md:pl-8 md:ml-8 md:border-l md:border-gray-200">
+                <a href="#_" class="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900">Home</a>
+                <a href="#_" class="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900">Amal</a>
+                <a href="#_" class="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900">About</a>
+                <a href="#_" class="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900">Contact</a>
+            </nav>
+        </div>
+
+        <div class="inline-flex items-center ml-5 space-x-6 lg:justify-end">
+            <a href="#"
+                class="text-base font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900">
+                Sign in
+            </a>
+            <a href="#"
+                class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-green-400 border border-transparent shadow-sm hover:bg-green-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-400 rounded-3xl">
+                Sign up
+            </a>
+        </div>
+    </div>
+</section>
